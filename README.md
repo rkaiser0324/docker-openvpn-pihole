3d4e14c6d6af0258297ef9ef365b74ff4ee2e9b1
Rolf Kaiser <1090569+rkaiser0324@users.noreply.github.com>
2019-11-10 06:29:04 -0800

    I think we can ignore the .rnd error
    Update setup instructions and clean up setup script

--- README.md
+++ README.md
@@ -1,26 +1,30 @@
 # docker-openvpn-pihole
-Create a single docker-compose and use the benefit from pi-hole if you use a Mobile Device outside your Home network.
+
+Create a single docker-compose and get the benefits of pi-hole on your mobile devices outside your home network.
 
 Rationale for this repository: I want to connect OpenVPN to Pi-Hole as easily as possible. However, the user can make changes (meaningful to me), or just use my preferences.
 
 Many thanks to:  
-GitHub @ [kylemanna/docker-openvpn](https://github.com/kylemanna/docker-openvpn/)  
-GitHub @ [pi-hole/docker-pi-hole](https://github.com/pi-hole/docker-pi-hole/)
+* GitHub @ [kylemanna/docker-openvpn](https://github.com/kylemanna/docker-openvpn/)  
+* GitHub @ [pi-hole/docker-pi-hole](https://github.com/pi-hole/docker-pi-hole/)
 
 Now you can use this repository with the Hardwaretype x86_x64 and amr (Test with Raspberry Pi 2)
 
 *YouTube: HowTo create this Container in about 4 Minutes*
 [![HowTo create this Container in about 4 Minutes](https://abload.de/img/screenshotcpjyo.jpg)](https://www.youtube.com/embed/8sRtCERYVzk)
 
-### [Changelog](https://github.com/mr-bolle/docker-openvpn-pihole/blob/master/CHANGELOG.md)
+[Changelog](https://github.com/mr-bolle/docker-openvpn-pihole/blob/master/CHANGELOG.md)
 
-### 1.1. Download and run install Script:
-```
+## Setup
+
+1. Set up a dynamic DNS hostname for your home network via Namecheap, FreeDNS, etc. 
+1. Configure your router to forward all traffic to that hostname, to the Docker container.
+1. Download and run install Script:
+```bash
 git clone https://github.com/mr-bolle/docker-openvpn-pihole.git
 cd docker-openvpn-pihole && bash openvpn-install.sh
 ```
-
-#### 1.2. OpenVPN create certificate and first user [Source](https://github.com/kylemanna/docker-openvpn/blob/master/docs/docker-compose.md)
+1. OpenVPN create certificate and first user [Source](https://github.com/kylemanna/docker-openvpn/blob/master/docs/docker-compose.md)
 
 Follow User Entry you have to made
 :bulb: **All [default] values can be accepted with ENTER** :bulb:
--- openvpn-install.sh
+++ openvpn-install.sh
@@ -1,18 +1,24 @@
 #!/bin/bash
-
-# Aurthor: Muhammad Asim
-# CoAuthor mr-bolle
-
-#Purpose: Setup OpenVPN in quick time. https://www.youtube.com/watch?v=NQpzIh7kSkY
-
-#We we are pulling the best Image of docker for OpenVPN on earth.
+#
+# Author: Muhammad Asim
+# CoAuthor: mr-bolle
+#
+# Instructions: https://www.youtube.com/watch?v=NQpzIh7kSkY
 
 set -euo pipefail
 
-echo -e "\nWe we are pulling the best Image of OpenVPN for docker on earth by kylemanna/openvpn\n"
+RED='\033[1;31m'
+GREEN='\033[0;32m'
+CYAN='\033[0;36m'
+YELLOW='\033[0;33m'
+BLUE='\033[0;34m'
+MAGENTA='\033[0;35m'
+NC='\033[0m' # No Color
+
+echo -e "${CYAN}This script uses \"the best image of OpenVPN for Docker on earth by kylemanna/openvpn\"...${NC}"
 
  if [ `uname -m` != 'x86_64' ]; then
-         echo "** Build a Docker Image from the kylemanna/openvpn repository **"
+         echo "Building Docker Image from the kylemanna/openvpn repository..."
          # docker build -t kylemanna/openvpn https://github.com/kylemanna/docker-openvpn.git
              git clone https://github.com/kylemanna/docker-openvpn.git && cd docker-openvpn
                  # change alpine image
@@ -23,29 +29,23 @@ echo -e "\nWe we are pulling the best Image of OpenVPN for docker on earth by ky
                       docker build --no-cache -t kylemanna/openvpn -f Dockerfile.aarch64 .
                  cd .. && rm -f -r docker-openvpn
      else
-         echo "** Pull the Docker Image from kylemanna/openvpn repository **"
+         echo "Pulling the Docker Image from kylemanna/openvpn repository..."
          docker pull kylemanna/openvpn
  fi
 
-
-#Step 1
-sleep 1
-
-echo -e "\nPerforming Step 1, we are going to make a directory at /openvpn_data\n"
+echo -e "${YELLOW}Creating directory at /openvpn_data...${NC}"
 
 mkdir -p $PWD/openvpn_data && OVPN_DATA=$PWD/openvpn_data
 
-echo -e "** OpenVPN Data Path is set to: $OVPN_DATA  **\n"
+echo -e "OpenVPN data path is set to: $OVPN_DATA"
 
 export OVPN_DATA
 
-sleep 1
-
 # OpenVPN dynDNS Domain (ex vpn.example.com:443)
-read -p "Please enter your dynDNS Address:            " IP
+read -p "Enter your dynamic DNS hostname and port (e.g., vpn.example.com:443): " IP
 
 # VPN Protocol 
-read -p "Please choose your Protocol (tcp / [udp]):   " PROTOCOL
+read -p "Choose your protocol (tcp / [udp]): " PROTOCOL
     
     if [ "$PROTOCOL" != "tcp" ]; then
 
@@ -90,22 +90,28 @@ read -p "Please enter the Pi-Hole Admin Password (default [$PIHOLE_PASSWORD_OLD]
         # echo -e "\n***********************************************************"             
             fi
 
-
-#Step 2
-echo -e "\nStep 2\n"
+echo -e "${YELLOW}Reading IPv4 from Pi-Hole container...${NC}"
 
 # read IPv4 from Pi-Hole Container 
 PIHOLE_IP=`grep 'ipv4' docker-compose.yml | awk ' NR==2 {print $2}'`
 docker run -v $OVPN_DATA:/etc/openvpn --rm kylemanna/openvpn ovpn_genconfig -n $PIHOLE_IP -u $PROTOCOL://$IP 
 # more Option: https://github.com/kylemanna/docker-openvpn/blob/master/bin/ovpn_genconfig
 
+echo -e "${YELLOW}Initializing PKI...${NC}"
 
-echo -e "\nAfter a Shortwhile You need to enter your Server Secure Password details please wait ...\n"
+#docker run -v $OVPN_DATA:/etc/openvpn --rm -it kylemanna/openvpn "ls -l /etc/"
 
-#Step 3
-sleep 3
+# Prevent SSL warning due to missing file - https://github.com/openssl/openssl/issues/7754
+# docker run -v $OVPN_DATA:/etc/openvpn --rm -it kylemanna/openvpn sed -i 's@easyrsa init-pki@easyrsa init-pki; touch /etc/openvpn/pki/.rnd ; ls -lsa /etc/openvpn/pki/ @g' /usr/local/bin/ovpn_initpki
 
-echo -e "\nWe are now at Step 3\n"
+
+
+# docker run -v $OVPN_DATA:/etc/openvpn --rm -it kylemanna/openvpn mkdir -p /etc/openvpn/pki
+# docker run -v $OVPN_DATA:/etc/openvpn --rm -it kylemanna/openvpn touch /etc/openvpn/pki/.rnd
+# docker run -v $OVPN_DATA:/etc/openvpn --rm -it kylemanna/openvpn chmod a+rw /etc/openvpn/pki/.rnd
+# docker run -v $OVPN_DATA:/etc/openvpn --rm -it kylemanna/openvpn ls -lsa /etc/openvpn/pki
+
+#echo -e "docker run -v $OVPN_DATA:/etc/openvpn --rm -it kylemanna/openvpn ovpn_initpki"
 docker run -v $OVPN_DATA:/etc/openvpn --rm -it kylemanna/openvpn ovpn_initpki
 
 #Step 4
